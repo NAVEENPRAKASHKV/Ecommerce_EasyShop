987c811cce9fd1516fe5874487968d068dd67de3
naveenprakashkv <naveen.prakash.kv@gmail.com>
2024-11-29 22:58:49 +0530

    shop page design completed with review and related product

---
+++ frontend/src/componets/Reviews.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import Ratings from "./Ratings";
+import RatingTemp from "./RatingTemp";
+import Pagination from "./Pagination";
+
+const Reviews = () => {
+  const [parPage, setParPage] = useState(1);
+  const [pageNumber, setPageNumber] = useState(10);
+  return (
+    <div className="mt-5  ">
+      {/* Rating summary */}
+      <div className=" gap-10 flex md:flex-col ">
+        <div className="">
+          <div className="flex flex-col gap-2 justify-center items-start">
+            <div>
+              <span className="font-bold text-[40px]">4.5</span>
+              <span className="font-bold text-[20px]">/</span>
+              <span className="font-bold text-[20px]">5</span>
+            </div>
+            <div className="flex justify-start items-center">
+              <Ratings ratings={4.5} />
+            </div>
+            <div>
+              <span>(34 reviews)</span>
+            </div>
+          </div>
+        </div>
+        {/* Rating detials */}
+        <div className="flex gap-2 flex-col py-2 ">
+          {/* rightside */}
+          <div className="flex flex-row items-center gap-5">
+            <div className="text-md flex gap-q w-[93px]">
+              <RatingTemp ratings={5} />
+            </div>
+            <div className="h-[14px] w-[200px] bg-slate-200">
+              <div className="h-full w-[60%] bg-yellow-400"></div>
+            </div>
+            <p className="text-sm">10</p>
+          </div>
+          <div className="flex flex-row items-center gap-5">
+            <div className="text-md flex gap-q w-[93px]">
+              <RatingTemp ratings={4} />
+            </div>
+            <div className="h-[14px] w-[200px] bg-slate-200">
+              <div className="h-full w-[70%] bg-yellow-400"></div>
+            </div>
+            <p className="text-sm">10</p>
+          </div>
+          <div className="flex flex-row items-center gap-5">
+            <div className="text-md flex gap-q w-[93px]">
+              <RatingTemp ratings={3} />
+            </div>
+            <div className="h-[14px] w-[200px] bg-slate-200">
+              <div className="h-full w-[10%] bg-yellow-400"></div>
+            </div>
+            <p className="text-sm">20</p>
+          </div>
+          <div className="flex flex-row items-center gap-5">
+            <div className="text-md flex gap-q w-[93px]">
+              <RatingTemp ratings={2} />
+            </div>
+            <div className="h-[14px] w-[200px] bg-slate-200">
+              <div className="h-full w-[20%] bg-yellow-400"></div>
+            </div>
+            <p className="text-sm">7</p>
+          </div>
+          <div className="flex flex-row items-center gap-5">
+            <div className="text-md flex gap-q w-[93px]">
+              <RatingTemp ratings={1} />
+            </div>
+            <div className="h-[14px] w-[200px] bg-slate-200">
+              <div className="h-full w-[30%] bg-yellow-400"></div>
+            </div>
+            <p className="text-sm">14</p>
+          </div>
+          <div className="flex flex-row items-center gap-5">
+            <div className="text-md flex gap-q w-[93px]">
+              <RatingTemp ratings={0} />
+            </div>
+            <div className="h-[14px] w-[200px] bg-slate-200">
+              <div className="h-full w-[40%] bg-yellow-400"></div>
+            </div>
+            <p className="text-sm">1</p>
+          </div>
+        </div>
+      </div>
+      {/* reviews */}
+      <div className="mt-5">
+        <h2 className="font-bold text-lg ">Product Reviews (23)</h2>
+        <div className="flex flex-col gap-8 pb-10 pt-4">
+          {[1, 2, 3, 4, 5].map((rev, index) => {
+            return (
+              <div>
+                <div className="flex justify-between">
+                  <div className="flex gap-1 ">
+                    <Ratings ratings={4} />
+                  </div>
+                  <span className="text-slate-600">8 jan 2024</span>
+                </div>
+                <div className="pb-3 pt-1">
+                  <h4 className="font-semibold pb-1">UserName</h4>
+                  <p>
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex justify-end">
+          {
+            <Pagination
+              pageNumber={pageNumber}
+              setPageNumber={setPageNumber}
+              totalItem={10}
+              parPage={parPage}
+              showItem={Math.floor(10 / 3)}
+            />
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,10 +1,59 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
 import Header from "./../componets/Header";
 import { Link } from "react-router-dom";
 import { FaAngleRight } from "react-icons/fa";
 import Footer from "../componets/Footer";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Ratings from "../componets/Ratings";
+import { FaHeart } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import Reviews from "../componets/Reviews";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Details = () => {
+  const images = [1, 2, 3, 4, 5, 6];
+  const [image, setImage] = useState("");
+  const [state, setState] = useState("reviews");
+  const discount = 5;
+  const stock = 3;
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 4,
+    },
+    mdtablet: {
+      breakpoint: { max: 800, min: 750 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 750, min: 600 },
+      items: 3,
+    },
+    xsmobile: {
+      breakpoint: { max: 610, min: 460 },
+      items: 2,
+    },
+    xxsmobile: {
+      breakpoint: { max: 460, min: 0 },
+      items: 2,
+    },
+  };
   return (
     <div>
       <Header />
@@ -41,7 +90,363 @@ const Details = () => {
       </section>
       {/* product details with product image and product details */}
       <section>
-        <div className="w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto"></div>
+        <div className="w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto">
+          <div className="grid grid-cols-2 md-lg:grid-cols-1 gap-8">
+            {/* product image layout */}
+            <div>
+              <div>
+                <img
+                  className="h-[400px] w-full"
+                  src={
+                    image
+                      ? `http://localhost:3000/images/products/${image}.webp`
+                      : `http://localhost:3000/images/products/${images[2]}.webp`
+                  }
+                  alt="images"
+                />
+                {/* carousel */}
+                <div className="my-8">
+                  {images && (
+                    <Carousel
+                      autoPlay={true}
+                      infinite={true}
+                      arrows={true}
+                      showDots={false}
+                      transitionDuration={500}
+                      responsive={responsive}
+                    >
+                      {images.map((img, index) => (
+                        <div key={index} onClick={() => setImage(img)}>
+                          <div className=" relative flex justify-center items-center ">
+                            <img
+                              src={`http://localhost:3000/images/products/${
+                                index + 1
+                              }.webp`}
+                              alt="images"
+                              className="h-[120px]   object-cover cursor-pointer "
+                            />
+                          </div>
+                        </div>
+                      ))}
+                    </Carousel>
+                  )}
+                </div>
+              </div>
+            </div>
+            {/* product details layout */}
+            <div className="flex flex-col justify-start">
+              <div>
+                <div className="flex flex-col justify-start gap-4">
+                  <h1 className="text-2xl font-bold ">Product Name</h1>
+                  {/* rating */}
+                  <div className="flex justify-start items-center gap-5">
+                    <div className="flex ">
+                      <Ratings ratings={4.5} />
+                    </div>
+
+                    <span>(32 reviews)</span>
+                  </div>
+                  {/* discount and price */}
+                  <div>
+                    {discount !== 0 ? (
+                      <div className="flex justify-start gap-3">
+                        <h2 className=" text-red-600 text-xl font-bold">
+                          Price
+                        </h2>
+                        <h2 className="line-through text-red-600 text-xl font-bold">
+                          ₹500
+                        </h2>
+                        <h2 className=" text-red-600 text-xl font-bold">
+                          ₹{500 - Math.floor((discount * 500) / 100)} (-
+                          {discount}%)
+                        </h2>
+                      </div>
+                    ) : (
+                      <div className="flex items-center gap-2">
+                        <h2 className=" text-red-600 text-xl font-bold">
+                          Price :
+                        </h2>
+                        <h2 className=" text-red-600 text-xl font-bold">
+                          ₹500
+                        </h2>
+                      </div>
+                    )}
+                  </div>
+                  {/* description */}
+                  <div className="text-slate-600">
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy text ever since the 1500s, when an unknown
+                      printer took a galley
+                    </p>
+                  </div>
+                  {/* count product & cart &  wishlist  */}
+                  <div className="flex justify-start items-center gap-4 mt-5">
+                    {stock ? (
+                      <div className="flex gap-4 ">
+                        <div className="w-[120px] bg-zinc-300 flex justify-center items-center gap-5 px-2 py-2 font-bold rounded-md text-xl">
+                          <button className="cursor-pointer w-[20px] h-[20px] flex justify-center rounded-full items-center hover:bg-zinc-600 hover:text-white">
+                            -
+                          </button>
+                          <div className="text-lg font-semibold">{2}</div>
+                          <button className="cursor-pointer w-[20px] h-[20px] flex justify-center rounded-full items-center hover:bg-zinc-600 hover:text-white">
+                            +
+                          </button>
+                        </div>
+                        <button className="px-8 py-3 h-[50px] cursor-pointer hover:shadow-lg hover:shadow-green-500/40 bg-[#059473] text-white">
+                          Add To Cart
+                        </button>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    <div className="h-[50px] w-[50px] flex justify-center items-center cursor-pointer hover:shadow-lg hover:shadow-cyan-500/40 bg-cyan-500 text-white">
+                      <FaHeart />
+                    </div>
+                  </div>
+                  {/* chat & buy & share and avilability */}
+                  <div className="flex flex-row  py-5 gap-5">
+                    {/* left side */}
+                    <div className="w-[150px] text-black font-bold text-base flex flex-col gap-5">
+                      <span>Availability</span>
+                      <span>Share On</span>
+                    </div>
+                    {/* right side */}
+                    <div className="flex  gap-5 flex-col">
+                      <span
+                        className={`${
+                          stock ? "text-green-800" : "text-red-500"
+                        } font-bold`}
+                      >
+                        {stock ? `In Stock(${stock})` : "Out Of Stock"}
+                      </span>
+                      <ul className="flex justify-start items-center gap-3">
+                        <li>
+                          <a
+                            className="w-[30px] h-[30px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-indigo-500 rounded-full text-white"
+                            href="#"
+                          >
+                            <FaFacebookF />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            className="w-[30px] h-[30px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-cyan-500 rounded-full text-white"
+                            href="#"
+                          >
+                            <FaTwitter />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            className="w-[30px] h-[30px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-purple-500 rounded-full text-white"
+                            href="#"
+                          >
+                            <FaLinkedin />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            className="w-[30px] h-[30px] hover:bg-[#059473] hover:text-white flex justify-center items-center bg-blue-500 rounded-full text-white"
+                            href="#"
+                          >
+                            <FaGithub />
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  {/* buy now and chat  */}
+                  <div className="flex gap-3">
+                    {stock ? (
+                      <button className="px-8 py-3 h-[50px] cursor-pointer hover:shadow-lg hover:shadow-green-500/40 bg-[#247462] text-white">
+                        Buy Now
+                      </button>
+                    ) : (
+                      ""
+                    )}
+                    <Link
+                      to="#"
+                      className="px-8 py-3 h-[50px] cursor-pointer hover:shadow-lg hover:shadow-red-500/40 bg-red-500 text-white"
+                    >
+                      Chat Seller
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* reviews & description & related products */}
+      <section>
+        <div className="w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto pb-16">
+          <div className="flex flex-wrap">
+            {/*  Description and Reviews */}
+            <div className="w-[72%] md-lg:w-full">
+              {/* button for description and reviews */}
+              <div className="grid grid-cols-2">
+                <button
+                  onClick={() => setState("reviews")}
+                  className={`bg-slate-500 text-white py-2  ${
+                    state === "reviews" ? "bg-green-800" : ""
+                  } `}
+                >
+                  Reviews
+                </button>
+                <button
+                  onClick={() => setState("description")}
+                  className={`bg-slate-500 text-white py-2  ${
+                    state === "description" ? "bg-green-800" : ""
+                  } `}
+                >
+                  Description
+                </button>
+              </div>
+              {/* reviews and description content */}
+              <div>
+                {state === "reviews" ? (
+                  <Reviews />
+                ) : (
+                  <p className="mt-10">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley Lorem Ipsum is simply dummy text of
+                    the printing and typesetting industry. Lorem Ipsum has been
+                    the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galleyLorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem
+                    Ipsum has been the industry's standard dummy text ever since
+                    the 1500s, when an unknown printer took a galleyLorem Ipsum
+                    is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy
+                    text ever since the 1500s, when an unknown printer took a
+                    galleyLorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley
+                  </p>
+                )}
+              </div>
+            </div>
+            {/* Related Products */}
+            <div className="w-[28%] md-lg:w-full">
+              <div className="pl-4 md-lg:pl-0">
+                <div className="bg-slate-500 py-2 text-white pl-4">
+                  <h2>From Easy Shop</h2>
+                </div>
+                <div className="flex flex-col gap-5 mt-3 border p-3">
+                  {[1, 2, 3].map((p, i) => {
+                    return (
+                      <div key={i} className="p-2 shadow-xl">
+                        <Link className="block">
+                          <div className="relative h-[200px] ">
+                            <img
+                              className="w-full h-full"
+                              src={`http://localhost:3000/images/products/${p}.webp`}
+                              alt=""
+                            />
+                            {discount !== 0 && (
+                              <div className="flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2">
+                                {discount}%
+                              </div>
+                            )}
+                          </div>
+
+                          <h2 className="text-slate-600 py-1 font-bold">
+                            Product Name
+                          </h2>
+                          <div className="flex gap-2">
+                            <h2 className="text-lg font-bold text-slate-600">
+                              $434
+                            </h2>
+                            <div className="flex items-center gap-2">
+                              <Ratings ratings={4.5} />
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Related Proudcts */}
+      <section>
+        <div className="w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto mb-10">
+          <h2 className="text-2xl py-8 text-slate-600">Related Products </h2>
+          <div>
+            <Swiper
+              slidesPerView="auto"
+              breakpoints={{
+                1280: {
+                  slidesPerView: 4,
+                },
+                565: {
+                  slidesPerView: 2,
+                },
+              }}
+              spaceBetween={25}
+              loop={true}
+              pagination={{
+                clickable: true,
+                el: ".custom_bullet",
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {[1, 2, 3, 4, 5, 6].map((p, i) => {
+                return (
+                  <SwiperSlide key={i}>
+                    <Link className="block shadow-2xl">
+                      <div className="relative h-[270px] flex flex-col ">
+                        {/* image */}
+                        <div className="w-full h-[270px]">
+                          <img
+                            className="h-[270px] w-full"
+                            src={`http://localhost:3000/images/products/${p}.webp`}
+                            alt=""
+                          />
+                          <div className="absolute h-full w-full top-0 left-0 bg-[#000] opacity-25 hover:opacity-50 transition-all duration-500"></div>
+                        </div>
+                        {/* discount */}
+                        {discount !== 0 && (
+                          <div className="flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2">
+                            {discount}%
+                          </div>
+                        )}
+                      </div>
+                      <div>
+                        {/* product details */}
+                        <div className="p-4 flex flex-col gap-1 text-black ">
+                          <h2 className="text-slate-600 text-lg font-bold">
+                            Product Name
+                          </h2>
+                          <div className="flex justify-start items-center gap-3 ">
+                            <h2 className="text-lg font-bold text-slate-600">
+                              $434
+                            </h2>
+                            <div className="flex">
+                              <Ratings ratings={4.5} />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+          <div className="w-full flex justify-center items-center py-8">
+            <div className="custom_bullet justify-center gap-3 !w-auto"></div>
+          </div>
+        </div>
       </section>
       <Footer />
     </div>
